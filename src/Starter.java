6abe34b0d1e633c0451459781312284f5150ff1f
Vivek <saivivekgorkal@gmail.com>
2022-04-30 22:18:04 -0400

    Added code for problems

--- src/Starter.java
+++ src/Starter.java
@@ -1,4 +1,18 @@
 package src;
 
 public class Starter {
+
+    public static int  fact(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        fact(n-1);
+        System.out.println(n);
+        fact(n-1);
+        return n;
+    }
+
+    public static void main(String[] args) {
+        fact(3);
+    }
 }
